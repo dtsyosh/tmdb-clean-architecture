f8c09ce5a6e8daf6a2ccfaa4c429ec5500c43077
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-16 16:52:11 -0400

    Added - missing param error custom

---
+++ src/presentation/errors/missing-param-error.ts
@@ -0,0 +1,8 @@
+class MissingParamError extends Error {
+  constructor(param: String) {
+    super(`The param ${param} is missing`);
+    this.name = 'MissingParamError';
+  }
+}
+
+export default MissingParamError;
