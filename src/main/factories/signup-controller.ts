5cb7db38fe628185555d3eabc8177e0b84d0193a
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-17 16:19:30 -0400

    Added - factory for signup controller

---
+++ src/main/factories/signup-controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from "@/presentation/contracts";
+import { MemoryAccountRepository } from "@/infra/repositories/cache";
+import SignupController from "@/presentation/controllers/signup";
+
+const makeSignupController = (): Controller => {
+  const repository = new MemoryAccountRepository();
+  const signupController = new SignupController(repository);
+
+  return signupController;
+}
