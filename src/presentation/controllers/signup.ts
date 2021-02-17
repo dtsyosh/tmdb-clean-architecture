25a2aaf8a697f5859f0e167415f30845db380d34
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-17 16:35:26 -0400

    Change - adapt to use new request type

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,6 @@
 import { badRequest, serverError, ok, forbidden, created } from '@/presentation/helpers'
 import { MissingParamError } from '@/presentation/errors';
-import { Controller, HttpResponse } from '@/presentation/contracts';
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/contracts';
 import { CreateAccount } from '@/domain/usecases/create-account';
 import { Account } from '@/domain/entities';
 
@@ -10,22 +10,24 @@ class SignupController implements Controller {
     private readonly usecase: CreateAccount
   ) { }
 
-  async handle(request: SignupController.Request): Promise<HttpResponse> {
-    if (!request.email) {
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    const { email, password, passwordConfirmation } = request.body;
+
+    if (email) {
       return badRequest(new MissingParamError('email'));
     }
 
-    if (!request.password) {
+    if (password) {
       return badRequest(new MissingParamError('password'));
     }
 
-    if (!request.passwordConfirmation) {
+    if (passwordConfirmation) {
       return badRequest(new MissingParamError('passwordConfirmation'));
     }
 
     const userData = {
-      email: request.email,
-      password: request.password
+      email,
+      password
     }
 
     const account: Account = await this.usecase.create(userData)
