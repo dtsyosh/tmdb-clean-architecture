cfc634c73beb58f573344469a7851987ff6e4374
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-17 16:39:19 -0400

    Added - export

--- src/main/factories/signup-controller.ts
+++ src/main/factories/signup-controller.ts
@@ -2,7 +2,7 @@ import { Controller } from "@/presentation/contracts";
 import { MemoryAccountRepository } from "@/infra/repositories/cache";
 import SignupController from "@/presentation/controllers/signup";
 
-const makeSignupController = (): Controller => {
+export const makeSignupController = (): Controller => {
   const repository = new MemoryAccountRepository();
   const signupController = new SignupController(repository);
 
