4903090867ebb9678174f4878814e88e937bde8b
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-18 15:18:39 -0400

    Added - dotenv

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,11 @@
+import { config } from 'dotenv';
 import { setupRoutes } from './routes';
-
 import express from 'express';
+
+config();
 
 const app = express();
 app.use(express.json());
 setupRoutes(app);
-
 
 export default app;
