d7ab8c0909e4238d97d6f3fe2fd72bfd8548541a
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-18 16:13:27 -0400

    Added - axios http client for tmdb

---
+++ src/infra/external/axios-http-client.spec.ts
@@ -0,0 +1,13 @@
+import { TMDBClient } from '@/infra/external';
+import { config } from 'dotenv';
+
+describe('TMDB Axios client', () => {
+  it('Should return a movie with the same id provided', async () => {
+    const id = 500;
+    const response = await TMDBClient.get(`/movie/${id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toHaveProperty('id');
+    expect(response.data.id).toBe(500);
+  });
+})
--- src/infra/external/axios-http-client.ts
+++ src/infra/external/axios-http-client.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { config } from 'dotenv';
+config();
 
 export const TMDBClient = axios.create({
   baseURL: process.env.TMDB_API_URL,
