e772e0b3f1857bf0916a11a2bec967c1e1da1db0
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-18 16:17:25 -0400

    Added - movies-repository test

--- src/infra/repositories/tmdb/movies-repository.spec.ts
+++ src/infra/repositories/tmdb/movies-repository.spec.ts
@@ -8,13 +8,27 @@ const makeSut = () => {
 }
 
 describe('TMDB Movies repository', () => {
+
   it('Should return a movie with id provided', async () => {
     const { sut } = makeSut();
 
-    const response = await sut.getMovieById(500);
+    const id = 500;
+
+    const response = await sut.getMovieById(id);
 
     expect(response.status).toBe(200);
     expect(response.data).toHaveProperty('id');
-    expect(response.data.id).toBe(500);
+    expect(response.data.id).toBe(id);
+  })
+
+  it('Should return a list of movies with query string provided', async () => {
+    const { sut } = makeSut();
+
+    const search = "Kong vs Godzilla";
+
+    const response = await sut.getMovie(search);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toHaveProperty('results');
   })
 })
