9d1814a5abb0b861b6ada47d9fa32f2cf5f1c948
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-24 13:23:33 -0400

    Changed - query method on supertest

--- __tests__/integration/search-movie-by-term.test.ts
+++ __tests__/integration/search-movie-by-term.test.ts
@@ -1,11 +1,13 @@
 import request from 'supertest';
 import app from '../../src/main/config/app';
 
-describe('GET /api/movies?term={term}', () => {
+describe('GET /api/movies', () => {
   it('Should return a list of movies when term is provided', async () => {
     const term = 'Fast & Furious'
 
-    const response = await request(app).get(`/api/movies?term=${term}`);
+    const response = await request(app)
+      .get('/api/movies')
+      .query({ term });
 
     expect(response.status).toBe(200);
     expect(response.body[0]).toHaveProperty('title');
