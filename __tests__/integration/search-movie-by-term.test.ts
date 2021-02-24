94a75d6605b2bcab748230a8531d52227817bd28
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2021-02-24 13:20:34 -0400

    Added - integration test with supertest

--- __tests__/integration/search-movie-by-term.test.ts
+++ __tests__/integration/search-movie-by-term.test.ts
@@ -1,32 +1,20 @@
-import { TMDBMoviesRepository } from '../../src/infra/repositories/tmdb';
-import { SearchMovieByTermController } from '../../src/presentation/controllers';
-import { SearchMovieByTermUseCase } from '../../src/data/usecases';
-import { TMDBClient } from '../../src/infra/external';
+import request from 'supertest';
+import app from '../../src/main/config/app';
 
-const makeSut = () => {
-  const movieRepository = new TMDBMoviesRepository(TMDBClient);
-  const usecase = new SearchMovieByTermUseCase(movieRepository);
-  const sut = new SearchMovieByTermController(usecase);
+describe('GET /api/movies?term={term}', () => {
+  it('Should return a list of movies when term is provided', async () => {
+    const term = 'Fast & Furious'
 
-  return {
-    sut
-  };
-}
+    const response = await request(app).get(`/api/movies?term=${term}`);
 
-describe('Search movie by term', () => {
-  it('Should return 200 when term is provided', async () => {
-    const { sut } = makeSut();
-
-    const httpRequest = {
-      query: {
-        term: 'kong vs godzilla'
-      }
-    }
-
-    const response = await sut.handle(httpRequest);
-
-
-    expect(response.statusCode).toBe(200);
+    expect(response.status).toBe(200);
     expect(response.body[0]).toHaveProperty('title');
-  })
+  });
+
+  it('Should return a badRequest when term is not provided in query', async () => {
+    const response = await request(app).get(`/api/movies`);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error');
+  });
 });
