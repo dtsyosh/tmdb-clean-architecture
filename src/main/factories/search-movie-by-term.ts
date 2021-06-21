2d3d2cb77c3b16d329fcf0e39a52be4e6c9e8c5f
Diego Tsuyoshi <diego@pointr.co>
2021-06-21 19:31:28 -0400

    Added - pretty quick

--- src/main/factories/search-movie-by-term.ts
+++ src/main/factories/search-movie-by-term.ts
@@ -1,15 +1,13 @@
-import { Controller } from '@/presentation/contracts';
-import { TMDBMoviesRepository } from '@/infra/repositories/tmdb';
-import { TMDBClient } from '@/infra/external';
-import { SearchMovieByTermUseCase } from '../../data/usecases';
-import { SearchMovieByTermController } from '../../presentation/controllers';
+import { TMDBClient } from "@/infra/external";
+import { TMDBMoviesRepository } from "@/infra/repositories/tmdb";
+import { Controller } from "@/presentation/contracts";
+import { SearchMovieByTermUseCase } from "../../data/usecases";
+import { SearchMovieByTermController } from "../../presentation/controllers";
 
 export const makeSearchByTermController = (): Controller => {
   const repository = new TMDBMoviesRepository(TMDBClient);
   const searchMovieByTermUseCase = new SearchMovieByTermUseCase(repository);
-  const searchMovieByTermController = new SearchMovieByTermController(
-    searchMovieByTermUseCase
-  );
+  const searchMovieByTermController = new SearchMovieByTermController(searchMovieByTermUseCase);
 
   return searchMovieByTermController;
-};
+}
--- src/main/factories/signup-controller.ts
+++ src/main/factories/signup-controller.ts
@@ -1,7 +1,7 @@
-import { Controller } from '@/presentation/contracts';
-import { MemoryAccountRepository } from '@/infra/repositories/cache';
-import { SignupController } from '@/presentation/controllers/signup';
-import { CreateAccountUseCase } from '@/data/usecases/create-account';
+import { CreateAccountUseCase } from "@/data/usecases/create-account";
+import { MemoryAccountRepository } from "@/infra/repositories/cache";
+import { Controller } from "@/presentation/contracts";
+import { SignupController } from "@/presentation/controllers/signup";
 
 export const makeSignupController = (): Controller => {
   const repository = new MemoryAccountRepository();
@@ -9,4 +9,4 @@ export const makeSignupController = (): Controller => {
   const signupController = new SignupController(createAccountUseCase);
 
   return signupController;
-};
+}
